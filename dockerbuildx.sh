48a42c65435a83efe16f64faf4b3cba711fd818b
Mesanga <eveikome@gmail.com>
2024-10-02 01:29:58 +0100

    dockerinstall script

---
+++ dockerbuildx.sh
@@ -0,0 +1,3 @@
+mkdir -p $HOME/.docker/cli-plugins
+wget -O $HOME/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.9.1/buildx-v0.9.1.linux-amd64
+chmod +x $HOME/.docker/cli-plugins/buildx
